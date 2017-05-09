19af01349a255c1e9a878ee736c45c11093e978b
Livadniy Alex <livalex11@gmail.com>
2017-05-10 02:01:03 +0300

    server add

---
+++ source/menu.js
@@ -0,0 +1,8 @@
+export default function (arr, className) {
+	const menu = document.createElement("ul");
+	menu.classList.add(className);
+	let listItems = '';
+	arr.forEach( item => {listItems += `<li>${item}</li>`;} );
+	menu.insertAdjacentHTML("beforeend", listItems);
+	return menu;
+}
