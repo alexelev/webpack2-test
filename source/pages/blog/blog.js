381b8c352c5cbf19d448bd3b13154dcaac1b8e0a
Livadniy Alex <livalex11@gmail.com>
2017-05-10 02:37:49 +0300

    added dev-server

--- source/pages/blog/blog.js
+++ source/pages/blog/blog.js
@@ -1,6 +1,6 @@
 import createMenu from "../../components/menu/menu.js";
 
-const menu = createMenu(["Главная", "Блог"], 'menu');
+const menu = createMenu(["Главная", "Блог", "Портфолио"], 'menu');
 
 document.body.appendChild(menu);
 console.log('blog page');
